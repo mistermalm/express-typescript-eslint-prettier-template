b1164d99caf662ccdc01bdfcc5839ab80363b649
mistermalm <jonatan.malm@live.se>
2025-03-19 12:52:04 +0100

    build(format): format check

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-const app = express();
-const port = '5000';
+import express from 'express'
+const app = express()
+const port = '5000'
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-  console.log('Response sent');
-});
+  res.send('Hello World!')
+  console.log('Response sent')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
